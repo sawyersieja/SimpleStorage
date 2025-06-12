83873f593134f6bac685416d5d60a5c4ca8cad53
Sawyer <121059512+sawyersieja@users.noreply.github.com>
2025-06-12 13:52:08 -0700

    Update SimpleStorage.sol
    Updated content from additional lessons.

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -38,7 +38,7 @@ contract SimpleStorage {
     // dynamic array
     Person[] public listOfPeople; //[]    
 
-
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     // view: reads state from blockchain, no tx required, disallows modification of state.
     // pure: disallow reading from state OR storange, AND disallows modification of state.
@@ -47,8 +47,10 @@ contract SimpleStorage {
         return myFavoriteNumber;
     }
 
+    // calldata, memory, storage
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push( Person(_favoriteNumber, _name) );
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 
 }
